39bec62aff45fcbc064a3b84910b8363fe4663a5
ohyuchan123 <oyuchan50@gmail.com>
2021-12-13 17:00:04 +0900

    study js

--- Vanilla/Js/app.js
+++ Vanilla/Js/app.js
@@ -1,1 +1,26 @@
-alert("hi");
+//(주석 해제 ctrl+k+u)
+//const는 constant(상수)라는 것이고, constant는 값이 바뀔 수 없다
+//var는 어떠한 규칙도 가지고 있지 않다
+// const a=2;
+let a=2;
+// var a=2;
+
+// const b=2;
+let b=2;
+// var b=2;
+
+// const myName = "yuchan";
+let myName = "yuchan";
+// var myName = "yuchan";
+
+// let const는 결과는 동일
+
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " +myName);
+
+myName = "ohyuchan";
+
+console.log("your new name is "+myName);
