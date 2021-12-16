d1e32ae8d720476e2b85d9cef191ffa1f06479d8
ohyuchan123 <oyuchan50@gmail.com>
2021-12-16 09:41:06 +0900

    study js

--- Vanilla/Study/BROWSER/EVENTS.js
+++ Vanilla/Study/BROWSER/EVENTS.js
@@ -1,4 +1,5 @@
-const title = document.querySelector(".hello h1");
+// const title = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
 
 console.log(title);
 
@@ -10,7 +11,20 @@ function handleTitleClick(){
     title.style.color="blue";
 }
 
+function handleMouseEnter(){
+    // console.log("mouse is here!");
+    title.innerText = "Mouse is here!"
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!";
+}
+
 //event를 듣는 것
 title.addEventListener("click",handleTitleClick);
+//mouse를 가져다 대면 Mouse is here!이라는 문자로 바꿈
+title.addEventListener("mouseenter",handleMouseEnter);
+//mouse를 가져다 대면 Mouse is gone!이라는 문자로 바꿈 
+title.addEventListener("mouseleave",handleMouseLeave);
 
 handleTitleClick()
