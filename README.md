804f4cd809181d3deac9681b2544c2a665027c92
ohyuchan123 <oyuchan50@gmail.com>
2021-12-17 13:42:59 +0900

    study js

---
+++ README.md
@@ -0,0 +1,5 @@
+# 오늘의 공부 내용 정리
+console.log에서 body에 매우 쉽게 접근 할 수 있다.  
+body는 특별해서 콘솔창에서 document.body로 호출이 가능하다
+
+EVENTS.js에서 창을 줄이면 색이 변한다
--- Vanilla/Study/BROWSER/EVENTS.js
+++ Vanilla/Study/BROWSER/EVENTS.js
@@ -1,30 +1,61 @@
-// const title = document.querySelector(".hello h1");
-const title = document.querySelector("div.hello:first-child h1");
+// const h1 = document.querySelector(".hello h1");
+// const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.log(title);
+// console.log(h1);
 
-// title.style.color = "blue";
+// h1.style.color = "blue";
 
-//title 클릭
+//h1 클릭
+// function handleh1Click(){
+//     // console.log("h1 was clicked!");
+// }
 function handleTitleClick(){
-    console.log("title was clicked!");
-    title.style.color="blue";
+    h1.style.color="blue";
+    // title.style.color="blue";
 }
 
 function handleMouseEnter(){
     // console.log("mouse is here!");
-    title.innerText = "Mouse is here!"
+    h1.innerText = "Mouse is here!";
+    // title.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
+    h1.innerText = "Mouse is gone!";
+    // title.innerText="Mouse is gone!";
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor="tomato";
+}
+function handleWindowCopy(){
+    //ctrl+c 누를 시 경고창을 띄움
+    alert("copier!");
+}
+function handleWindowOffline(){
+    //인터넷이 끊켰을 때 경고창 생성
+    alert("SOS no WIFI");
+}
+function handleWindowOnline(){
+    alert("ALL GOOOD");
 }
 
-//event를 듣는 것
-title.addEventListener("click",handleTitleClick);
-//mouse를 가져다 대면 Mouse is here!이라는 문자로 바꿈
-title.addEventListener("mouseenter",handleMouseEnter);
-//mouse를 가져다 대면 Mouse is gone!이라는 문자로 바꿈 
-title.addEventListener("mouseleave",handleMouseLeave);
 
-handleTitleClick()
+// h1.onclick("click",handleh1Click);
+//event를 듣는 것
+// title.addEventListener("click",handleh1Click);  
+h1.addEventListener("click",handleTitleClick);   
+// h1.onclick = handleh1Click;
+//mouse를 가져다 대면 Mouse is here!이라는 문자로 바꿈
+// title.addEventListener("mouseenter",handleMouseEnter);
+h1.addEventListener("mouseenter",handleMouseEnter);
+// h1.onmouseenter= handleMouseEnter;
+//mouse를 가져다 대면 Mouse is gone!이라는 문자로 바꿈 
+// title.addEventListener("mouseleave",handleMouseLeave);
+h1.addEventListener("mouseleave",handleMouseLeave);
+
+// handleh1Click();
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener("copy",handleWindowCopy);
+window.addEventListener("offline",handleWindowOffline);
+window.addEventListener("online",handleWindowOnline);
