c2871ba0587959663bacce292e5b1d7d7a5d1860
ohyuchan123 <oyuchan50@gmail.com>
2021-12-20 16:31:33 +0900

    study js

--- Vanilla/Js/app.js
+++ Vanilla/Js/app.js
@@ -1,7 +1,10 @@
 //(주석 해제 ctrl+k+u)
 const loginForm = document.querySelector("#login-form");
 const logininput= document.querySelector("#login-form input");
-const link = document.querySelector("a");
+// const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
 // const loginButton = document.querySelector("#login-form button");
 
 // function onLoginBtnClick(){
@@ -19,18 +22,23 @@ const link = document.querySelector("a");
 function onLoginSubmit(event){
     //어떤 이벤트를 명시적으로 처리하지 않은 경우 해당 이벤트에 대한 사용자 에이전트의 기본 동작을 하지 않게 한다
     event.preventDefault();
-    console.log(event);
-}
-function handleLinckClick(event){
-    event.preventDefault();
     // console.log(event);
-    console.dir(event);
-    // alert("clicked");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = logininput.value;
+    greeting.innerText="Hello "+username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
+// function handleLinckClick(event){
+//     event.preventDefault();
+//     // console.log(event);
+//     console.dir(event);
+//     // alert("clicked");
+// }
 
 // loginButton.addEventListener("click",onLoginBtnClick);
 loginForm.addEventListener("submit",onLoginSubmit);
-link.addEventListener("click",handleLinckClick);
+// link.addEventListener("click",handleLinckClick);
 
 
 
