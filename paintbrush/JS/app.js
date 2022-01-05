5afc5cfebb703d54687dcfb5f599a9b13ba78eb5
ohyuchan123 <oyuchan50@gmail.com>
2022-01-05 20:21:34 +0900

    looking for error

--- paintbrush/JS/app.js
+++ paintbrush/JS/app.js
@@ -1,16 +1,41 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
-function onMouseEnter(event){
-    const x= event.offsetX;
-    const y =event.offsety;
+function stopPainting(){
+    painting = false;
 }
-function onmousedown(event){
+
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y =event.offsety;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event){
     painting = true;
 }
 
 if(canvas){
-    canvas.addEventListener("mousemove",onMouseEnter);
-    canvas.addEventListener("mosedown",onmousedown);
+    canvas.addEventListener("mousemove",onMouseMove);
+    canvas.addEventListener("mosedown",startPainting);
+    canvas.addEventListener("mouseup",stopPainting);
+    canvas.addEventListener("mouseleave",stopPainting);
 }
